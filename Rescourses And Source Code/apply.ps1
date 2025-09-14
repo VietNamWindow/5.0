d8c81e3e9685809e58d8923c5610f9d7aa34bf8a
VietNamWindow <1hxngzitx@gmail.com>
2025-09-14 15:53:49 +0700

    Add files via upload

---
+++ Rescourses And Source Code/apply.ps1
@@ -0,0 +1,12 @@
+$imgUrl = "https://i.postimg.cc/XY3MJ768/Frame-14.png"
+
+$imgPath = "$env:TEMP\wallpaper.jpg"
+Invoke-WebRequest -Uri $imgUrl -OutFile $imgPath
+Add-Type @"
+using System.Runtime.InteropServices;
+public class Wallpaper {
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}
+"@
+[Wallpaper]::SystemParametersInfo(20, 0, $imgPath, 3)
